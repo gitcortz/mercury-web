bba3423d25471a56fa62d5fff494b494aa7eff75
mcortz <mykortz@gmail.com>
2019-04-06 16:20:43 +0800

    initial components  TopNav, SideNav, Footer

---
+++ src/components/layout/SideNav/SideNav.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SideNav extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>this is side nav</p>;
+  }
+}
+
+export default SideNav;
---
+++ src/components/layout/TopNav/TopNav.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TopNav extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p> this is top nav</p>;
+  }
+}
+
+export default TopNav;
