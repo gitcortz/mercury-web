b82718ca30635cfd77cdda8f0e77e317b29d5404
mcortz <mykortz@gmail.com>
2019-04-06 21:52:59 +0800

    update component SideNav add component Dashboard

--- src/components/layout/SideNav/SideNav.js
+++ src/components/layout/SideNav/SideNav.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
 
 class SideNav extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    return <p>this is side nav</p>;
+    return <div>side nav here</div>;
   }
 }
 
