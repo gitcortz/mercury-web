92a52c04b37fce80a520f639c08f4c6ecbcc1db5
mcortz <mykortz@gmail.com>
2019-04-06 21:54:10 +0800

    Add Router, Add components in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
+import TopNav from "./components/layout/TopNav";
+import Footer from "./components/layout/Footer";
+import SideNav from "./components/layout/SideNav";
+import Dashboard from "./components/dashboard/Dashboard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My react app</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <SideNav />
+          <TopNav />
+          <Route exact path="/" component={Dashboard} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
