ea7d86cc9007d62ffeafd7780090561c96c25bd1
mcortz <mykortz@gmail.com>
2019-05-12 02:14:53 +0800

    update css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -13,19 +13,54 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
 import "./App.css";
+import "./custom.css";
 import PrivateRoute from "./components/common/PrivateRoute";
 import AddContact from "./components/company/contact/AddContact";
+import UpdateContact from "./components/company/contact/UpdateContact";
+import PurchaseOrder from "./components/purchasing/PurchaseOrder";
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    sidebar: () => <div>home!</div>,
+    main: () => <h2>Home</h2>
+  },
+  {
+    path: "/purchase/order",
+    sidebar: () => <div>bubblegum!</div>,
+    main: () => <h2>Bubblegum</h2>
+  },
+  {
+    path: "/shoelaces",
+    sidebar: () => <div>shoelaces!</div>,
+    main: () => <h2>Shoelaces</h2>
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
+          {/*routes.map(route => (
+            <Route
+              key={route.path}
+              path={route.path}
+              exact={route.exact}
+              component={route.sidebar}
+            />
+          ))*/}
           <div className="App page-wrapper mercury-theme">
             <SideNav />
             <div className="page-content">
               <TopNav />
               <Route exact path="/contact/create" component={AddContact} />
+              <Route
+                exact
+                path="/contact/update/:id"
+                component={UpdateContact}
+              />
               <Route exact path="/" component={Login} />
               <Route exact path="/register" component={Register} />
               <Switch>
@@ -33,6 +68,7 @@ class App extends Component {
               </Switch>
               <Route exact path="/contact" component={Contacts} />
               <Route exact path="/sales/order" component={SalesOrder} />
+              <Route exact path="/purchase/order" component={PurchaseOrder} />
               <Footer />
             </div>
           </div>
